91faacd594ac082b6913b016fbbba48e7ac60813
jhordan.followize <jhordan.suzuki@followize.com.br>
2023-10-08 13:31:42 -0300

    feat: adicionando documentação docBlock.

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -2,6 +2,7 @@
 
 namespace App\Core;
 
+
 class Router
 {
 
@@ -15,16 +16,31 @@ class Router
         $this->response = $response;
     }
 
+    /**
+     * Add a GET route.
+     *
+     * @param string $path
+     * @param mixed $callback
+     */
     public function get($path, $callback)
     {
         $this->routes['get'][$path] = $callback;
     }
 
+    /**
+     * Add a POST route.
+     *
+     * @param string $path
+     * @param mixed $callback
+     */
     public function post($path, $callback)
     {
         $this->routes['post'][$path] = $callback;
     }
 
+    /**
+     * Resolves the Current route.
+     */
     public function resolve()
     {
         $path = $this->request->getPath();
@@ -42,32 +58,33 @@ class Router
             return $this->renderView($callback);
         }
 
-        return dd(call_user_func($callback));
+        $this->executeController($callback);
     }
 
-    public function renderView($view)
+
+    /**
+     *  Render the view with the layout.
+     *
+     * @param string $view Name.
+     * @param array $params of view.
+     */
+    public function renderView($view, $params = [])
     {
 
         $layoutContent = $this->layoutContent();
-        $viewContent = $this->renderOnlyView($view);
+        $viewContent = $this->renderOnlyView($view, $params);
 
         $path =  str_replace('{{content}}', $viewContent, $layoutContent);
 
         echo $path;
     }
 
-    // public function renderContent($view)
-    // {
 
-    //     $layoutContent = $this->layoutContent();
-
-    //     $viewContent = $this->renderOnlyView($view); 
-
-    //     $path =  str_replace('{{content}}', $viewContent, $layoutContent);
-
-    //     echo $path;
-    // }
-
+    /**
+     * Get the content of the layout.
+     *
+     * @return string layout content.
+     */
     protected function layoutContent()
     {
         ob_start();
@@ -79,8 +96,21 @@ class Router
         return $output;
     }
 
-    public function renderOnlyView($view)
+    /**
+     * Render just view content.
+     *
+     * @param string $view name.
+     * @param array $params view.
+     * @return string Content View.
+     */
+
+    public function renderOnlyView($view, $params)
     {
+
+        foreach ($params as $key => $value) {
+            $$key = $value;
+        }
+
         ob_start();
 
         include_once "../src/Views/$view.php";
@@ -88,5 +118,40 @@ class Router
         $output = ob_get_clean();
 
         return $output;
+    }
+
+
+    /**
+     * Runs the specified controlle.
+     *
+     * @param array $callback Callback of Controller.
+     */
+
+    public function executeController($callback)
+    {
+        $controller = $callback[0];
+        $methodName = $callback[1];
+       
+
+        if (class_exists($controller) && method_exists($controller, $methodName)) {
+            $controllerInstance = new $controller();
+            $method = $this->request->getMethod();
+
+            switch ($method) {
+                case 'get':
+                  
+                    $controllerInstance->$methodName();
+
+                    break;
+
+                case 'post':
+                    $controllerInstance->$methodName($this->request);
+
+                    break;
+            }
+        } else {
+
+            echo   "Classe ou método não encontrados";
+        }
     }
 }
